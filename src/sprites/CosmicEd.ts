81642f570c44c0506f4b333fe58ee3e57f84e219
mikecules <michael.moncada@gmail.com>
2017-09-25 12:57:37 -0400

    * tweak to id

--- src/sprites/CosmicEd.ts
+++ src/sprites/CosmicEd.ts
@@ -1,7 +1,7 @@
 import * as Phaser from 'phaser-ce';
 
 class CosmicEd extends Phaser.Sprite {
-  public static COSMIC_ED_ID = 'cosmic_id';
+  public static COSMIC_ED_ID = 'cosmic_ed';
 }
 
 export default CosmicEd;
